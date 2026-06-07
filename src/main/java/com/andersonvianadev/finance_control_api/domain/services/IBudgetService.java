974f4a45e83a97b5a8cf8e33192891cdfbcd7a8b
AndersonDev <andersonp.viana.contato@gmail.com>
2026-06-07 16:18:11 -0300

    feat/category-expense-limits: create a method to find budget by id

--- src/main/java/com/andersonvianadev/finance_control_api/domain/services/IBudgetService.java
+++ src/main/java/com/andersonvianadev/finance_control_api/domain/services/IBudgetService.java
@@ -1,7 +1,11 @@
 package com.andersonvianadev.finance_control_api.domain.services;
 
 import com.andersonvianadev.finance_control_api.domain.models.Budget;
+import com.andersonvianadev.finance_control_api.domain.models.User;
+
+import java.util.UUID;
 
 public interface IBudgetService {
     Budget save(Budget budget);
+    Budget findById(User owner, UUID id);
 }
