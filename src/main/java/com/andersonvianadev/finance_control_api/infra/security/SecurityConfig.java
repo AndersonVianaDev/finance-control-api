535f5b863b283213f4ce5e5ced5c126eb66f6c07
AndersonDev <andersonp.viana.contato@gmail.com>
2026-08-04 19:56:17 -0300

    financial-summary/feat: remove correlationId

--- src/main/java/com/andersonvianadev/finance_control_api/infra/security/SecurityConfig.java
+++ src/main/java/com/andersonvianadev/finance_control_api/infra/security/SecurityConfig.java
@@ -50,8 +50,4 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean
-    public CorrelationIdFilter correlationIdFilter() {
-        return new CorrelationIdFilter();
-    }
 }
